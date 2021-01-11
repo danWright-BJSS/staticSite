fe00488558f97a70e8d657afe95fb1d1d3bb486b
Daniel Wright <daniel.wright@bjss.com>
2021-01-11 16:53:16 -0600

    ar

--- src/containers/ar/index.js
+++ src/containers/ar/index.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-require('./aframe');
-require('./aframe-ar');
+
+
+
 
 export default function ARContainer() {
 
     return ( 
         <div>
+          <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
 
-<a-scene embedded arjs>
+    <a-scene embedded arjs>
       <a-marker preset="hiro">
         <a-entity
           position="0 0 0"
           scale="0.05 0.05 0.05"
-          gltf-model="../../../resources/building/assets/avatar_image.png"
+          gltf-model="../../../resources/building/assets/avatar1.glb"
         ></a-entity>
       </a-marker>
       <a-entity camera></a-entity>
